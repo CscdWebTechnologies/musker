6df697441e83cb5229ccdafc5c7fa88013ea24cb
Dzah Yaw <82473101+Let-Love-Lead@users.noreply.github.com>
2021-09-22 16:40:09 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import MidSection from "./components/MidAfNav";
+import Table from "./components/Table";
+import End from "./components/End";
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+          <Navbar />
+          <MidSection />
+          <Table />
+          <End />
+    </div>
+  );
+}
+
+export default App;
+  
